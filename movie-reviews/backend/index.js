60b2180fa14b9bd51eebc67df2685e8ec6f2263b
Jeff Rosengarden <jeff@rosengarden.com>
2021-12-29 17:19:54 -0600

    Adding detailed comments for future reference as I'm learning MERN

--- movie-reviews/backend/index.js
+++ movie-reviews/backend/index.js
@@ -1,30 +1,33 @@
 // 12/29/21 Jeff Rosengarden
+// for connecting to the database and starting the server
 
-import app from './server.js'
-import mongodb from "mongodb"
-import dotenv from "dotenv"
+import app from './server.js'                       // import app created in server.js
+import mongodb from "mongodb"                       // declared in package.json
+import dotenv from "dotenv"                         // declared in package.json
 import MoviesDAO from './dao/moviesDAO.js'
  
-async function main(){        
-     dotenv.config()    
+async function main(){                              // create async function main() to connect to mongoDB cluster
+                                                    // and call functions that access the database
+     dotenv.config()                                // load the environment variables from .env                          
     
-const client = new mongodb.MongoClient(
+const client = new mongodb.MongoClient(             // create instance of MongoClient and pass URI retrieved from .env
 process.env.MOVIEREVIEWS_DB_URI
     )
-    const port = process.env.PORT || 8000     
+    const port = process.env.PORT || 8000           // retrieve the port from .env and if inaccessible use port 8000
      try {
         // Connect to the MongoDB cluster
-        await client.connect()
+        await client.connect()                      // now wait until a connection to the database is made
 	await MoviesDAO.injectDB(client)
  
-        app.listen(port, () =>{
-            console.log('server is running on port:'+port);        
-        })
+        app.listen(port, () =>{                     // if connected with no issues the web server is started
+            console.log('server is running on port:'+port);        // and displays this message
+        })                                                          // and is now listening on the specified port
  
     } catch (e) {
-        console.error(e);        
-        process.exit(1)
+        console.error(e);                           // if connection failed display error on console                            
+        process.exit(1)                             // and terminate
     } 
 }
  
-main().catch(console.error);
+main().catch(console.error);                        // main is now implemented so it's called and any errors are
+                                                    // sent to the console
--- movie-reviews/backend/server.js
+++ movie-reviews/backend/server.js
@@ -1,17 +1,27 @@
 // 12/26/21 Jeff Rosengarden MBPro Silicon
 
-import express from 'express'
-import cors from 'cors'
-import movies from './api/movies.route.js'
+
+// import  middleware
+import express from 'express'                               // needed middleware installed in package.json
+import cors from 'cors'                                     // needed middleware installed in package.json
+import movies from './api/movies.route.js'                  // 
  
-const app = express()
+const app = express()                                       // create the server named app
  
-app.use(cors())
-app.use(express.json())
+app.use(cors())                                             // attach both cors and express.json middleware
+app.use(express.json())                                     // express.json is JSON parsing middleware to enable server
+                                                            // to read/accept JSON in a request's body
+
+                                                            // middleware are functions that Express executes AFTER
+                                                            // the incoming request and BEFORE the output.  Middlewares
+                                                            // might also make changes to a request/response object
  
-app.use("/api/v1/movies", movies)
-app.use('*', (req,res)=>{
-     res.status(404).json({error: "not found"})
+app.use("/api/v1/movies", movies)                           // specify the initial routes using standard protocol
+app.use('*', (req,res)=>{                                   // any other route (which won't exist) will return the
+     res.status(404).json({error: "not found"})             // "not found" message
 })
  
-export default app
+export default app                                          // app (our server) is now exported as a module so that other
+                                                            // files can import it since this is the file that will access
+                                                            // the database and starts the server.  Doing this allows 
+                                                            // seperating main server code from database code 
